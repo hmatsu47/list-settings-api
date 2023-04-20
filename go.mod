9404da5931c359c1e4f67e457dcebb02de914fdb
hmatsu47 <hmatsu47@gmail.com>
2023-04-21 00:37:09 +0900

    CORS 実装

--- go.mod
+++ go.mod
@@ -29,6 +29,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ecr v1.18.9
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.21.1 // indirect
--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	middleware "github.com/deepmap/oapi-codegen/pkg/gin-middleware"
 	"github.com/hmatsu47/list-settings-api/api"
 )
 
-func NewGinListSettingsServer(listSettings *api.ListSettings, port int) *http.Server {
+func NewGinListSettingsServer(listSettings *api.ListSettings, port int, origins []string) *http.Server {
 	swagger, err := api.GetSwagger()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Swagger specの読み取りに失敗しました\n: %s", err)
@@ -26,6 +28,25 @@ func NewGinListSettingsServer(listSettings *api.ListSettings, port int) *http.Se
 
 	// Gin Router 設定
 	r := gin.Default()
+
+	// CORS 設定
+	r.Use(cors.New(cors.Config{
+	AllowOrigins: origins,
+	AllowMethods: []string{
+		"GET",
+		"OPTIONS",
+	},
+	AllowHeaders: []string{
+		"Access-Control-Allow-Credentials",
+		"Access-Control-Allow-Headers",
+		"Content-Type",
+		"Content-Length",
+		"Accept-Encoding",
+		"Authorization",
+	},
+	AllowCredentials: true,
+	MaxAge: 24 * time.Hour,
+	}))
 
 	// HTTP Request の Validation 設定
 	r.Use(middleware.OapiRequestValidator(swagger))
@@ -44,18 +65,22 @@ func main() {
 	port := flag.Int("port", 28080, "Port for API server")
 	configPathPrefix := ""
 	tagRepositoryUri := ""
+	var origins []string
 	flag.Parse()
-	if flag.NArg() == 0 {
-		panic("タグの情報が指定されていません")
+	if flag.NArg() < 4 {
+		panic("必要なパラメーターが指定されていません")
 	}
 	// タグと対応する環境名はコマンドラインパラメータで取得
 	var tagKeys []api.TagKey
 	for i, v := range flag.Args() {
-		if i == 0 {
-			configPathPrefix = v
-		} else if i == 1 {
-			tagRepositoryUri = v
-		} else {
+		switch i {
+			case 0:
+				origins = strings.Split(v, ",")
+			case 1:
+				configPathPrefix = v
+			case 2:
+				tagRepositoryUri = v
+			default:
 			tag := api.TagKey{
 				TagName:         strings.Split(v, ":")[0],
 				EnvironmentName: strings.Split(v, ":")[1],
@@ -68,7 +93,7 @@ func main() {
 	}
 	// Server Instance 生成
 	listSettings := api.NewListSettings(configPathPrefix, tagRepositoryUri, &tagKeys)
-	s := NewGinListSettingsServer(listSettings, *port)
+	s := NewGinListSettingsServer(listSettings, *port, origins)
 	// 停止まで HTTP Request を処理
 	log.Fatal(s.ListenAndServe())
 }
